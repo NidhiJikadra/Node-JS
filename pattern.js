52028a969f488d5184b29552973ab2d0951b191b
Nidhi Jikadra <nidhijikadra3103@gmail.com>
2023-09-13 17:57:06 +0530

    Done

--- pattern.js
+++ pattern.js
@@ -1,10 +1,14 @@
 let n = 5;
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n - i; j++) {
-    process.stdout.write(' ')
+for (let i = 0; i < n; i++) {
+ 
+  for (let j = 0; j < n-i; j++) {
+    document.write('&nbsp ')
   }
-  for (let k = 0; k < 2 * i - 1; k++) {
-    process.stdout.write('*')
+  for (let l = 1; l<=i; l++) {
+    document.write('*')
   }
-  console.log();
+  for (let k = 0; k<=i; k++) {
+    document.write('*')
+  }
+  document.write("<br>")
 }
